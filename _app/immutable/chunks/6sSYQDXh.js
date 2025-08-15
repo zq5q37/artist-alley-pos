47bf770e7539a60fbee22ee592951e532adcd0c0
zq5q37 <117970574+zq5q37@users.noreply.github.com>
2025-08-15 18:13:43 +0800

    Updates

---
+++ _app/immutable/chunks/6sSYQDXh.js
@@ -0,0 +1,1 @@
+import{V as a}from"./BwRTE3Yh.js";a();
---
+++ _app/immutable/nodes/4.DxrZP8G4.js
@@ -0,0 +1,2 @@
+import"../chunks/DsnmJJEf.js";import"../chunks/6sSYQDXh.js";import{a5 as a,a9 as o,ae as r}from"../chunks/BwRTE3Yh.js";var n=a(`<h2>Settings</h2> <div class="card svelte-1cemvnq">Bundle rules are currently hard-coded to business policy.
+    Expose toggles here if you want to change them live.</div>`,1);function p(e){var t=n();r(2),o(e,t)}export{p as component};
